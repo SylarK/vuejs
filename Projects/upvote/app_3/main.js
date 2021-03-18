1143b5511fe0b7ab8d5e9166d582d18e29c6e742
SylarK <syst.lucasamado@gmail.com>
2021-03-18 19:54:42 -0300

    Reactive functions

--- Projects/upvote/app_3/main.js
+++ Projects/upvote/app_3/main.js
@@ -11,5 +11,17 @@ const upvoteApp = {
       });
     },
   },
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+        //Why I decide use to find.
+        //The up-voting logic involves using the native JavaScript find()³⁶ method to locate the submission
+        //object with the id equal to the submissionId parameter. The votes attribute of that submission is
+        //then incremented by one.
+      );
+      submission.votes++;
+    },
+  },
 };
 Vue.createApp(upvoteApp).mount('#app');
