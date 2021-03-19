b380ecbf6497325e6fc58d5f516e5d9da640920e
SylarK <syst.lucasamado@gmail.com>
2021-03-19 20:47:00 -0300

    Create the product description drawer with a background that is works like an overlay in this project. And the drawer.. that contains the description and some methods

--- Projects/shoppingCart_app/shoppingcart/src/views/Home.vue
+++ Projects/shoppingCart_app/shoppingcart/src/views/Home.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="home">
+    <ProductDescriptionDrawer
+      :product="product"
+      :active="active.product_drawer"
+      v-on:close-product-drawer="closeProductDrawer"
+    />
+
     <div class="product-cards-container">
       <ProductSummaryCard
         v-for="product in items"
         :key="product.id"
         :product="product"
-      ></ProductSummaryCard>
+        v-on:view-product="viewProduct($event)"
+      />
     </div>
   </div>
 </template>
 
 <script>
 //importing
-import items from '../data/items';
-import ProductSummaryCard from '../components/products/ProductsSummaryCard.vue';
+import items from "../data/items";
+import ProductSummaryCard from "../components/products/ProductsSummaryCard.vue";
+import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     ProductSummaryCard,
+    ProductDescriptionDrawer,
   },
   data() {
     return {
       items: items,
+      product: null,
+      active: {
+        product_drawer: false,
+      },
     };
+  },
+  methods: {
+    viewProduct(product) {
+      this.active.product_drawer = true;
+      this.product = product;
+      console.log(this.product);
+    },
+    closeProductDrawer() {
+      this.active.product_drawer = false;
+    },
   },
 };
 </script>
