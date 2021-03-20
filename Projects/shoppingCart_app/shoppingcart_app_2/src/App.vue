c624139fd8a8cf9edb95629cd6a4c995145749ee
SylarK <syst.lucasamado@gmail.com>
2021-03-20 11:29:15 -0300

    New shoppingcart project with vuejs using vuex

---
+++ Projects/shoppingCart_app/shoppingcart_app_2/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container">
+    <nav-bar></nav-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
