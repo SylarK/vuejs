aca43f7f66c7e26986d5e9b4b80f74ce6767bfcd
SylarK <syst.lucasamado@gmail.com>
2021-03-20 12:33:27 -0300

    Calling the vuex in the productsvue. It's working

--- Projects/shoppingCart_app/shoppingcart_app_2/src/components/NavBar.vue
+++ Projects/shoppingCart_app/shoppingcart_app_2/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    TESTE
+    NAVBAR
   </div>
 </template>
--- Projects/shoppingCart_app/shoppingcart_app_2/src/components/Products.vue
+++ Projects/shoppingCart_app/shoppingcart_app_2/src/components/Products.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    <h2>Template Products</h2>
+    <h1 class="title">All Products</h1>
+    <p>{{ length }} products</p>
+    <table class="table is-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  computed: mapGetters({
+    products: 'allProducts',
+    length: 'getNumbersOfProducts',
+  }),
+  methods: mapActions(['addToCart']),
+};
+</script>
