2a456e21c17d8b94c5d404becf7efa79e29a540d
SylarK <syst.lucasamado@gmail.com>
2021-03-20 11:03:21 -0300

    Start a new project of shopping cart using boostrap-vue

---
+++ Projects/shoppingCart_app/shoppingcart_app_2/src/main.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/boostrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import BoostrapVue from 'bootstrap-vue';
+Vue.use(BoostrapVue);
+createApp(App).mount('#app');
