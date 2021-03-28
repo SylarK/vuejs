34b321a5f9eff809a1e53d1fffe27b706c9a2cfe
Lucas A <syst.lucasamado@gmail.com>
2021-03-28 18:23:07 -0300

    Applying vuex paradigm in the form

--- Projects/formhandling_app/app/07-vuex_paradigm/main.js
+++ Projects/formhandling_app/app/07-vuex_paradigm/main.js
@@ -3,133 +3,159 @@ const InputForm = {
     <div class="input-form">
       <form @submit="submitForm" class="ui form">
         <div class="field">
-          <label>New note</label>
-          <!--<input ref="newItem" type="text" placeholder="Add an item!" />-->
-          <input v-model="fields.newItem" type="text" placeholder="Add an item!" />
+          <label>New Item</label>
+          <input :value="newItem" @input="onInputChange"
+            name="NEW_ITEM" type="text" placeholder="Add an item!" />
+          <span style="float: right">{{ newItemLength }}/20</span>
           <span style="color: red">{{ fieldErrors.newItem }}</span>
           <span v-if="isNewItemInputLimitExceeded"
-                style="color:red; display:block">
-                Must be under twenty characters
+            style="color: red; display: block">
+            Must be under twenty characters
           </span>
         </div>
         <div class="field">
           <label>Email</label>
-          <input
-            v-model="fields.email"
-            type="text"
-            placeholder="What's your email?"
-          />
+          <input :value="email" @input="onInputChange"
+            name="EMAIL" type="text" placeholder="What's your email?" />
           <span style="color: red">{{ fieldErrors.email }}</span>
         </div>
         <div class="field">
           <label>Urgency</label>
-          <select v-model="fields.urgency" class="ui fluid search dropdown">
+          <select :value="urgency" @change="onInputChange"
+            name="URGENCY" class="ui fluid search dropdown">
             <option disabled value="">Please select one</option>
             <option>Nonessential</option>
             <option>Moderate</option>
             <option>Urgent</option>
           </select>
           <span style="color: red">{{ fieldErrors.urgency }}</span>
-          <span v-if="isNotUrgent" 
-            style="color: red; display:block"
-          >
+          <span v-if="isNotUrgent"
+            style="color: red; display: block">
             Must be moderate to urgent
           </span>
         </div>
         <div class="field">
           <div class="ui checkbox">
-            <input v-model="fields.termsAddConditions" type="checkbox" />
-            <label> I accept the terms and conditions </label>
-            <span style="color: red">{{ fieldErrors.termsAddConditions }}</span>
+            <input :checked="termsAndConditions" @change="onInputChange"
+              name="TERMS_AND_CONDITIONS" type="checkbox" />
+            <label>I accept the terms and conditions</label>
+            <span style="color: red">
+              {{ fieldErrors.termsAndConditions }}
+            </span>
           </div>
         </div>
-        <button 
-          v-if="saveStatus === 'SAVING'"
-          disabled class="ui button"
-        >Saving...
+        <button v-if="saveStatus === 'SAVING'"
+          disabled class="ui button">
+          Saving...
         </button>
-        <button 
-          v-if="saveStatus === 'SUCCESS'"
+        <button v-if="saveStatus === 'SUCCESS'"
           :disabled="isNewItemInputLimitExceeded || isNotUrgent"
           class="ui button">
           Saved! Submit another
         </button>
-        <button 
-          v-if="saveStatus === 'ERROR'"
+        <button v-if="saveStatus === 'ERROR'"
           :disabled="isNewItemInputLimitExceeded || isNotUrgent"
-          class="ui button"
-        >Save Failed - Retry?
+          class="ui button">
+          Save Failed - Retry?
         </button>
-        <button 
-          v-if="saveStatus === 'READY'"
+        <button v-if="saveStatus === 'READY'"
           :disabled="isNewItemInputLimitExceeded || isNotUrgent"
-          class="ui button"
-        >Submit
+          class="ui button">
+          Submit
         </button>
       </form>
       <div class="ui segment">
-        <h4 class="ui header">Notes</h4>
+        <h4 class="ui header">Items</h4>
         <ul>
           <div v-if="loading" class="ui active inline loader"></div>
-          <li v-for="item in items" class="item"> {{ item }}</li>
+          <li v-for="item in items" class="item">{{ item }}</li>
         </ul>
       </div>
-    </div>
-  `,
+    </div>`,
   data() {
     return {
-      fields: {
-        newItem: '',
-        email: '',
-        urgency: '',
-        termsAddConditions: false,
-      },
       fieldErrors: {
         newItem: undefined,
         email: undefined,
         urgency: undefined,
-        termsAddConditions: undefined,
+        termsAndConditions: undefined,
       },
-      items: [],
       loading: false,
       saveStatus: 'READY',
-      //possibilites for saveStatus:
-      // READY, SAVING, SUCCESS, ERROR
     }
   },
+  created() {
+    this.loading = true
+    this.$store
+      .dispatch('loadItems')
+      .then((response) => {
+        this.loading = false
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  computed: Vuex.mapGetters({
+    newItem: 'newItem',
+    newItemLength: 'newItemLength',
+    isNewItemInputLimitExceeded: 'isNewItemInputLimitExceeded',
+    email: 'email',
+    urgency: 'urgency',
+    isNotUrgent: 'isNotUrgent',
+    termsAndConditions: 'termsAndConditions',
+    items: 'items',
+  }),
   methods: {
+    onInputChange(evt) {
+      const element = evt.target
+      const value =
+        element.name === 'TERMS_AND_CONDITIONS'
+          ? element.checked
+          : element.value
+      this.$store.commit(`UPDATE_${element.name}`, value)
+    },
     submitForm(evt) {
       evt.preventDefault()
-      this.fieldErrors = this.validateForm(this.fields)
+
+      this.fieldErrors = this.validateForm(this.$store.state.fields)
       if (Object.keys(this.fieldErrors).length) return
 
-      const items = [...this.items, this.fields.newItem]
+      const items = [
+        ...this.$store.state.items,
+        this.$store.state.fields.newItem,
+      ]
 
       this.saveStatus = 'SAVING'
-      apiClient
-        .saveItems(items)
+
+      this.$store
+        .dispatch('saveItems', items)
         .then(() => {
-          this.items = items
-          this.fields.newItem = ''
-          this.fields.email = ''
-          this.fields.urgency = ''
-          this.fields.termsAddConditions = false
           this.saveStatus = 'SUCCESS'
         })
-        .catch((e) => {
-          console.log(e)
+        .catch((err) => {
+          console.log(err)
           this.saveStatus = 'ERROR'
         })
     },
     validateForm(fields) {
       const errors = {}
-      if (!fields.newItem) errors.newItem = 'New Item Required'
-      if (!fields.email) errors.email = 'Email Required'
-      if (!fields.urgency) errors.urgency = 'Urgency Required'
-      if (!fields.termsAddConditions)
-        errors.termsAddConditions = 'Terms and conditions have to be approved'
+      if (!this.$store.state.fields.newItem) {
+        errors.newItem = 'New Item Required'
+      }
+      if (!this.$store.state.fields.email) {
+        errors.email = 'Email Required'
+      }
+      if (!this.$store.state.fields.urgency) {
+        errors.urgency = 'Urgency Required'
+      }
+      if (!this.$store.state.fields.termsAndConditions) {
+        errors.termsAndConditions = 'Terms and conditions have to be approved'
+      }
 
-      if (fields.email && !this.isEmail(fields.email)) {
+      if (
+        this.$store.state.fields.email &&
+        !this.isEmail(this.$store.state.fields.email)
+      ) {
         errors.email = 'Invalid Email'
       }
 
@@ -137,56 +163,15 @@ const InputForm = {
     },
     isEmail(email) {
       const re = /\S+@\S+\.\S+/
-
       return re.test(email)
     },
   },
-  computed: {
-    isNewItemInputLimitExceeded() {
-      return this.fields.newItem.length >= 20
-    },
-    isNotUrgent() {
-      return this.fields.urgency === 'Nonessential'
-    },
-  },
-  created() {
-    ;(this.loading = true),
-      apiClient.loadItems().then((items) => {
-        this.items = items
-        this.loading = false
-      })
-  },
-}
-
-// API CLIENT
-let apiClient = {
-  loadItems: function () {
-    return {
-      then: function (cb) {
-        setTimeout(() => {
-          cb(JSON.parse(localStorage.items || '[]'))
-        }, 1000)
-      },
-    }
-  },
-  saveItems: function (items) {
-    const success = !!(this.count++ % 2)
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (!success) return reject({ success })
-
-        localStorage.items = JSON.stringify(items)
-        return resolve({ success })
-      }, 1000)
-    })
-  },
-
-  count: 1,
 }
 
 Vue.createApp({
   components: {
     'input-form': InputForm,
   },
-}).mount('#app')
+})
+  .use(store)
+  .mount('#app')
